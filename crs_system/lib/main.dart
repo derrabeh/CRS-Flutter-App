e182794213e6e5c818ef214ebc2ee7b879f75d0c
Gan Jun An <junangan00@gmail.com>
2021-04-01 13:08:01 +0800

    add provider

--- crs_system/lib/main.dart
+++ crs_system/lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:crs_system/pages/apply_trip_page.dart';
+import 'package:crs_system/pages/document_detail_page.dart';
 import 'package:crs_system/pages/manage_profile_page.dart';
 import 'package:crs_system/pages/document_add_page.dart';
 import 'package:crs_system/pages/signup_page.dart';
+import 'package:crs_system/pages/trip_add_page.dart';
 import 'package:crs_system/pages/volunteer_page.dart';
 import 'package:crs_system/pages/volunteer_document_page.dart';
 import 'package:crs_system/providers/application_provider.dart';
 import 'package:crs_system/providers/document_provider.dart';
+import 'package:crs_system/providers/trip_provider.dart';
 import 'package:crs_system/providers/user_provider.dart';
 import 'package:crs_system/providers/volunteer_provider.dart';
 import 'package:flutter/material.dart';
@@ -32,6 +35,9 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(
           create: (context) => VolunteerProvider(),
         ),
+        ChangeNotifierProvider(
+          create: (context) => TripProvider(),
+        )
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
@@ -43,6 +49,7 @@ class MyApp extends StatelessWidget {
           DocumentPage.routeName:(context) => DocumentPage(),
           AddDocumentPage.routeName:(context) => AddDocumentPage(),
           ApplyTripPage.routeName:(context) => ApplyTripPage(),
+          ManageDocumentPage.routeName:(context) => ManageDocumentPage(),
         },
       ),
     );
