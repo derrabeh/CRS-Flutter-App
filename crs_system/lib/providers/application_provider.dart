dd7bab45fb17c4122db69e8a832e17145b69bf42
Pramila18 <40681809+Pramila18@users.noreply.github.com>
2021-04-02 12:56:19 +0800

    Update application_provider.dart

--- crs_system/lib/providers/application_provider.dart
+++ crs_system/lib/providers/application_provider.dart
@@ -1,13 +1,16 @@
 import 'dart:convert';
-import 'package:crs_system/models/application.dart';
-import 'package:crs_system/models/user.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:crs_system/models/document.dart';
+import 'package:crs_app/models/document.dart';
+import 'package:crs_app/models/application.dart';
+import 'package:crs_app/models/user.dart';
+import 'package:crs_app/models/volunteer.dart';
+import 'package:crs_app/models/user.dart';
+import 'package:crs_app/providers/user_provider.dart';
+import 'package:crs_app/models/trip.dart';
 
 class ApplicationProvider with ChangeNotifier {
   List<Application> _applicationList = [];
-  User currentUser;
 
 
   List<Application> get applicationList{
@@ -22,6 +25,62 @@ class ApplicationProvider with ChangeNotifier {
     return _applicationList.firstWhere((application) => application.applicationID == id);
   }
 
+
+  Volunteer currentVolunteer;
+  //volunteerID is application volunteerID
+  //this function is to set the current volunteer
+  Future<Volunteer> getVolunteerFromApplicationVolunteerID(String volunteerID) async{
+    String url = 'https://crs1-ae1ae-default-rtdb.firebaseio.com/volunteers.json';
+    try{
+      final response = await http.get(url);
+      final extractedData = json.decode(response.body) as Map<String, dynamic>;
+      if(extractedData.length>0) {
+        extractedData.forEach((volunteerID,volunteerData) {
+          Volunteer newVolunteer = Volunteer(
+            volunteerId: volunteerID,
+            userId: volunteerData['userId'],
+          );
+          if(newVolunteer.volunteerId==volunteerID){
+            currentVolunteer = newVolunteer;
+          }
+        });
+        notifyListeners();
+      }
+      return currentVolunteer;
+    }catch(error){
+      print(error);
+    }
+  }
+
+  User currentUser;
+  //this function is using currentVolunteer to set the currentUser
+  //userID is currentUser.userID
+  Future<void> getUserFromVolunteer(String userID) async{
+    String url = 'https://crs1-ae1ae-default-rtdb.firebaseio.com/users.json';
+    try{
+      final response = await http.get(url);
+      final extractedData = json.decode(response.body) as Map<String, dynamic>;
+      if(extractedData.length>0) {
+        extractedData.forEach((userID,userData) {
+          User newUser = User(
+            id: userID,
+            username: userData['username'],
+            password: userData['password'],
+            email: userData['email'],
+            address: userData['address'],
+            phone: userData['phone'],
+          );
+          if(newUser.id == userID){
+            currentUser = newUser;
+          }
+        });
+        notifyListeners();
+      }
+    }catch(error){
+      print(error);
+    }
+  }
+
   Future<void> getAllApplication() async{
     String url = 'https://crs1-ae1ae-default-rtdb.firebaseio.com/applications.json';
     try{
@@ -31,12 +90,12 @@ class ApplicationProvider with ChangeNotifier {
       if(extractedData.length>0) {
         extractedData.forEach((applicationID,applicationData) {
           Application newApplication = Application(
-              applicationID: applicationID,
-              applicationDate : applicationData['applicationDate'],
-              status: applicationData['status'],
-              remarks: applicationData['remarks'],
-              volunteerID: applicationData['volunteerID'],
-              tripID: applicationData['tripID'],
+            applicationID: applicationID,
+            applicationDate : applicationData['applicationDate'],
+            status: applicationData['status'],
+            remarks: applicationData['remarks'],
+            volunteerID: applicationData['volunteerID'],
+            tripID: applicationData['tripID'],
           );
           loadingApplication.add(newApplication);
         });
@@ -47,6 +106,39 @@ class ApplicationProvider with ChangeNotifier {
       print(error);
     }
   }
+  Future<void> getAllApplicationForAdmin(String tripID) async{
+    String url = 'https://crs1-ae1ae-default-rtdb.firebaseio.com/applications.json';
+
+    try{
+      final response = await http.get(url);
+      final extractedData = json.decode(response.body) as Map<String, dynamic>;
+      final List<Application> loadingApplication = [];
+      if(extractedData.length>0) {
+        extractedData.forEach((applicationID,applicationData) {
+
+          Application newApplication = Application(
+            applicationID: applicationID,
+            applicationDate : applicationData['applicationDate'],
+            status: applicationData['status'],
+            remarks: applicationData['remarks'],
+            volunteerID: applicationData['volunteerID'],
+            tripID: applicationData['tripID'],
+          );
+          if(applicationData['tripID'] == tripID ) {
+           if (applicationData['status'] == 'NEW' ||
+               applicationData['status'] == 'ACCEPTED') {
+              loadingApplication.add(newApplication);
+            }
+          }
+        });
+        _applicationList = loadingApplication;
+        notifyListeners();
+      }
+    }catch(error){
+      print(error);
+    }
+  }
+
 
   Future<Document> addApplication(Application application) async {
     String url = 'https://crs1-ae1ae-default-rtdb.firebaseio.com/applications.json';
@@ -64,6 +156,8 @@ class ApplicationProvider with ChangeNotifier {
           applicationDate: application.applicationDate,
           status: application.status,
           remarks: application.remarks
+
+
       );
       _applicationList.add(newApplication);
       notifyListeners();
@@ -84,6 +178,11 @@ class ApplicationProvider with ChangeNotifier {
             'volunteerID' : application.volunteerID,
             'tripID' : application.tripID
           }));
+
+      //  if (application.status == 'NEW' ||
+      //      application.status == 'ACCEPTED') {
+
+
       _applicationList[applicationIndex] = application;
       notifyListeners();
     }catch(error){
